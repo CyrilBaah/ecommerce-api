f0a609e98b7b599613f73aca55bcb46facbf2a0a
CyrilBaah <cyril.baah@amalitech.org>
2021-08-21 14:08:37 +0000

    Database - Set up

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 require('dotenv').config();
+const { sequelize } = require('./models');
 
 app.use(express.json());
 app.use(morgan('tiny'));
@@ -14,4 +15,6 @@ app.get('/', (req, res) => {
 const PORT = process.env.PORT;
 app.listen(PORT, async () => {
     console.log(`Server running on http://localhost:${PORT}`);
+    sequelize.authenticate();
+    console.log('Database synced');
 });
